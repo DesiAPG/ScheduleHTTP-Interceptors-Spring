8aebef74034f999664ca2f4311da40c6fc2d8279
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-04 23:36:52 -0500

    Create MVCConfig.java

---
+++ src/main/java/com/example/schedule/MVCConfig.java
@@ -0,0 +1,21 @@
+package com.example.schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MVCConfig implements WebMvcConfigurer {
+
+    @Autowired
+    @Qualifier("schedule")
+    private HandlerInterceptor schedule;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(schedule).excludePathPatterns("/closed");
+    }
+}
